5865bdc46d6f4c7c1a6daa844266399a0030d0ca
Gleiverson Vinagre <148836750+Gleiverson@users.noreply.github.com>
2025-10-10 10:50:15 -0300

    Update Departamento.java

--- src/main/java/com/example/empresa/model/Departamento.java
+++ src/main/java/com/example/empresa/model/Departamento.java
@@ -1,1 +1,20 @@
+package com.exemplo.empresa.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Departamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String localizacao;
+}
 
