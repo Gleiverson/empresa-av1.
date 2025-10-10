f8225c62de8364ffba8c73c33c7f8e6cb58f8984
Gleiverson Vinagre <148836750+Gleiverson@users.noreply.github.com>
2025-10-10 10:52:22 -0300

    Update FuncionarioRepository.java

--- src/main/java/com/example/empresa/repository/FuncionarioRepository.java
+++ src/main/java/com/example/empresa/repository/FuncionarioRepository.java
@@ -1,1 +1,10 @@
+package com.exemplo.empresa.repository;
+
+import com.exemplo.empresa.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+}
 
