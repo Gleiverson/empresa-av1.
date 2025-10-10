3422b5f2caf467d739f8c64c471f4577b95c44aa
Gleiverson Vinagre <148836750+Gleiverson@users.noreply.github.com>
2025-10-10 10:52:00 -0300

    Update DepartamentoRepository.java

--- src/main/java/com/example/empresa/repository/DepartamentoRepository.java
+++ src/main/java/com/example/empresa/repository/DepartamentoRepository.java
@@ -1,1 +1,10 @@
+package com.exemplo.empresa.repository;
+
+import com.exemplo.empresa.model.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+}
 
