7cb39b551159103b81c46fd36b2c729275346263
Gleiverson Vinagre <148836750+Gleiverson@users.noreply.github.com>
2025-10-10 14:26:14 -0300

    Update WebController.java

--- src/main/java/com/example/empresa/controller/WebController.java
+++ src/main/java/com/example/empresa/controller/WebController.java
@@ -1,1 +1,15 @@
+package com.example.empresa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index() {
+        // procura um arquivo chamado "index.html" na pasta resources/templates
+        return "index";
+    }
+}
 
